15d16627c7b84af3384eb3192478566de1d0bf4f
Kenny <kennethichoi@gmail.com>
2018-10-03 22:21:23 -0700

    Completed section 1-4 of Udemy course, will begin section on CSS architecture next.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+gulp.task('default', function() {
+    console.log("Hurrah, you created a gulp task!");
+});
+
+gulp.task('html', function() {
+    console.log('imagine something useful being done to your HTML here');
+});
+
+gulp.task('styles', function() {
+    return gulp.src('./assets/styles/style.css')
+    .pipe(postcss([nested, cssvars, autoprefixer]))
+    .pipe(gulp.dest('./temp/styles'));
+    
+});
+
+gulp.task('watch', function() {
+    
+    watch('./index.html', function() {
+        gulp.start('html');
+    });
+
+    watch('./assets/styles/**/*.css', function() {
+        gulp.start('styles');
+    });
+});
+
+
