6ea3353288b189b59d9bf7c8845e93f3e0c78dad
Kenny <kennethichoi@gmail.com>
2018-10-04 20:09:31 -0700

    Completed What is BEM? section

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function() {
     console.log("Hurrah, you created a gulp task!");
@@ -15,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
     return gulp.src('./assets/styles/style.css')
-    .pipe(postcss([nested, cssvars, autoprefixer]))
+    .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
     .pipe(gulp.dest('./temp/styles'));
     
 });
