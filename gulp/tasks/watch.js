0a2efa941c9b636bc7aaea5c17d159a7ee89c35e
Kenny <kennethichoi@gmail.com>
2018-10-05 21:44:40 -0700

    Completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+    
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: './'
+        }
+    });
+    watch('./index.html', function() {
+        browserSync.reload();
+    });
+
+    watch('./assets/styles/**/*.css', function() {
+        gulp.start('cssInject');
+    });
+});
+
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./temp/styles/style.css')
+    .pipe(browserSync.stream());
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,12 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-    console.log("Hurrah, you created a gulp task!");
-});
 
-gulp.task('html', function() {
-    console.log('imagine something useful being done to your HTML here');
-});
 
-gulp.task('styles', function() {
-    return gulp.src('./assets/styles/style.css')
-    .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
-    .pipe(gulp.dest('./temp/styles'));
-    
-});
 
-gulp.task('watch', function() {
-    
-    watch('./index.html', function() {
-        gulp.start('html');
-    });
 
-    watch('./assets/styles/**/*.css', function() {
-        gulp.start('styles');
-    });
-});
+
+
+
 
 
